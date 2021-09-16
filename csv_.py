7adc2b9ba5278285899e020fa0469a76775069e4
connor <chanwick@gmail.com>
2021-09-16 12:55:14 -0400

    added regex and sys files

--- csv_.py
+++ csv_.py
@@ -1,1 +1,31 @@
- import csv
+# import csv
+# import sys
+
+# with open(sys.argv[1], 'rt') as f:
+#     reader = csv.reader(f)
+#     val = 0
+
+#     for row in reader:
+#         if val < 5: #this is so we just see first 5 lines -- lose the if to see all lines
+#             print(row)
+#             val += 1
+
+## https://stackoverflow.com/questions/53172771/how-to-take-csv-file-as-an-input
+## same thing as above but using argparse
+
+import sys
+import argparse
+import csv
+from os import read
+
+parser = argparse.ArgumentParser('csv', description="Taking in csv filepath")
+parser.add_argument('csv', help="enter the filename--make sure the file's in the program directory")
+args = parser.parse_args()
+
+with open(args.csv, 'rt') as csvFile:
+    val = 0
+    reader = csv.reader(csvFile)
+    for row in reader:
+        if val < 5: #this is so we just see first 5 lines -- lose the if to see all lines
+            print(row)
+            val += 1
---
+++ sys_argv_.py
@@ -0,0 +1,7 @@
+import csv
+import sys
+
+def main(arg):
+    print(int(arg) * 14)
+
+main(sys.argv[1])
